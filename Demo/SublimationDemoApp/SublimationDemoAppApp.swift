ce558a94f1f8bae73e35160a55a3492740695cd3
Leo Dion <leogdion@brightdigit.com>
2022-11-14 19:56:26 -0500

    working on migrating to new app

---
+++ Demo/SublimationDemoApp/SublimationDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SublimationDemoAppApp.swift
+//  SublimationDemoApp
+//
+//  Created by Leo Dion on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SublimationDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
