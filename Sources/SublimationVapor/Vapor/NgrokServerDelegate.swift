c90d6df602944616577df9ef976eaeaf0177392f
Leo Dion <leogdion@brightdigit.com>
2022-11-17 20:17:18 -0500

    fixing weak delegate

--- Sources/SublimationVapor/Vapor/NgrokServerDelegate.swift
+++ Sources/SublimationVapor/Vapor/NgrokServerDelegate.swift
@@ -1,6 +1,6 @@
 import Ngrokit
 
-public protocol NgrokServerDelegate {
+public protocol NgrokServerDelegate : AnyObject {
   func server(_ server: NgrokServer, updatedTunnel tunnel: NgrokTunnel)
   func server(_ server: NgrokServer, errorDidOccur error: Error)
   func server(_ server: NgrokServer, failedWithError error: Error)
