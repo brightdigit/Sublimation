671731ac9fcacac5476cf9ada61663ba3ba18452
Leo Dion <leogdion@brightdigit.com>
2024-06-21 12:44:41 -0400

    Adding Improved organization

---
+++ Sources/Sublimation/Sublimation+Bonjour.swift
@@ -0,0 +1,74 @@
+//
+//  Sublimation+Bonjour.swift
+//  Sublimation
+//
+//  Created by Leo Dion.
+//  Copyright © 2024 BrightDigit.
+//
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the “Software”), to deal in the Software without
+//  restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following
+//  conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#if canImport(Network)
+  import Network
+
+  @_exported import class SublimationBonjour.BonjourDepositor
+  import class SublimationBonjour.BonjourSublimatory
+
+  extension Sublimation {
+    public convenience init(
+      listenerParameters: NWParameters = .tcp,
+      serviceType: String = BonjourSublimatory.httpTCPServiceType,
+      maximumCount: Int? = nil,
+      addresses: @escaping @Sendable () async -> [String],
+      addressFilter: @escaping @Sendable (String) -> Bool = String.isIPv4NotLocalhost(_:)
+    ) {
+      let sublimatory = BonjourSublimatory(
+        listenerParameters: listenerParameters,
+        serviceType: serviceType,
+        maximumCount: maximumCount,
+        addresses: addresses,
+        addressFilter: addressFilter
+      )
+      self.init(
+        sublimatory: sublimatory
+      )
+    }
+
+    #if os(macOS)
+      public convenience init(
+        listenerParameters: NWParameters = .tcp,
+        serviceType: String = BonjourSublimatory.httpTCPServiceType,
+        maximumCount: Int? = nil,
+        addressFilter: @escaping @Sendable (String) -> Bool = String.isIPv4NotLocalhost(_:)
+      ) {
+        self.init(
+          listenerParameters: listenerParameters,
+          serviceType: serviceType,
+          maximumCount: maximumCount,
+          addresses: BonjourSublimatory.addressesFromHost,
+          addressFilter: addressFilter
+        )
+      }
+
+    #endif
+  }
+#endif
